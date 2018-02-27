192adfd8d6884c5525d16f6cd1dd22e3c9aa7399
zmj4948 <zmj4948@rit.edu>
2018-02-27 17:39:51 -0500

    created the startup interface that allows the users to choose between the Normal Mode and the meme mode, and the start up pages for both modes

--- app/src/main/java/com/example/zjame/decisionmakingapp/MainActivity.java
+++ app/src/main/java/com/example/zjame/decisionmakingapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.zjame.decisionmakingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void meme(View view){
+        Intent intent=new Intent(this, MemeMain.class);
+        startActivity(intent);
+    }
+    public void normal(View view){
+        Intent intent=new Intent(this, NormalMain.class);
+        startActivity(intent);
+    }
+
 }
